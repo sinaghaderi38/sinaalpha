c5bb410efb34176521b297f553188cbfcaede768
sinaghaderi38 <sinaghaderi38@gmail.com>
2016-02-14 15:06:28 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {176727452},
+    sudo_users = {160147766},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
